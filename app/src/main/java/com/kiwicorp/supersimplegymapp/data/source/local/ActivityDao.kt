905ae6642bf946a423d8e0842eb52f4aafbc4a4b
kevintram <thekevintram@gmail.com>
2020-10-31 08:30:59 -0400

    fixes activities with the starting with the same first letter but different case being separated

--- app/src/main/java/com/kiwicorp/supersimplegymapp/data/source/local/ActivityDao.kt
+++ app/src/main/java/com/kiwicorp/supersimplegymapp/data/source/local/ActivityDao.kt
@@ -6,13 +6,13 @@ import com.kiwicorp.supersimplegymapp.data.Activity
 
 @Dao
 interface ActivityDao {
-    @Query("SELECT * FROM activities ORDER BY activity_name")
+    @Query("SELECT * FROM activities ORDER BY activity_name COLLATE NOCASE")
     fun observeActivities(): LiveData<List<Activity>>
 
-    @Query("SELECT * FROM activities ORDER BY activity_name")
+    @Query("SELECT * FROM activities ORDER BY activity_name COLLATE NOCASE")
     suspend fun getActivities(): List<Activity>
 
-    @Query("SELECT * FROM activities WHERE activity_name LIKE '%' || :query || '%' ORDER BY activity_name")
+    @Query("SELECT * FROM activities WHERE activity_name LIKE '%' || :query || '%' ORDER BY activity_name COLLATE NOCASE")
     suspend fun getSearchedActivities(query: String?): List<Activity>
 
     @Query("SELECT * FROM activities WHERE activity_id = :activityId")
